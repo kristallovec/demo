79eba694f12170d7bedf8c663fd346d1497de898
Ivan <example@gmail.com>
2022-03-21 17:21:59 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const answer = [];
+
+answer[0] = prompt('Vashe imya?', '');
+answer[1] = prompt('skolko let?', '');
+answer[2] = prompt('Kto ti?', '');
+
+document.write(answer);
+ 
